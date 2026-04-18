2f27af7ffa5714a6e6fcf78569678ee58a0d160e
AvinashD24 <nerdtech264@gmail.com>
2026-04-18 13:47:18 -0700

    Basic VR code

---
+++ vr-code/XRA_hackathon/XRA_hackathon/XRA_hackathonApp.swift
@@ -0,0 +1,18 @@
+//
+//  XRA_hackathonApp.swift
+//  XRA_hackathon
+//
+//  Created by iguest on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct XRA_hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
